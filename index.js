45c6ce0f1fb7efebad496a060455b03c708cdefb
Ben Lee <benlee.thatsme@gmail.com>
2022-01-08 17:02:04 +0000

    tidy up and set cron to 7:55

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const shell = require("shelljs");
 const { differenceInDays, isBefore, addDays, format } = require("date-fns");
 const { letters } = require("./letters");
 
+const NUMBER_OF_COMMITS = 100;
+const message = "BEN LEE!!";
+const user = { name: "Ben Lee", email: "benlee.thatsme@gmail.com" };
+
 if (!shell.which("git")) {
   shell.echo("Sorry, this script requires git");
   shell.exit(1);
@@ -14,24 +18,25 @@ const startingSunday = new Date(
 );
 
 if (isBefore(today, startingSunday)) {
-  shell.echo("Not yet at next starting sunday");
+  shell.echo(`Cycle starts again on ${format(startingSunday, "yyyy-MM-dd")}`);
   shell.exit(0);
 }
 
-shell.exec(`git config user.name "Ben Lee"`);
-shell.exec(`git config user.email "benlee.thatsme@gmail.com"`);
-
-const message = "BEN LEE!!";
+shell.exec(`git config user.name "${user.name}"`);
+shell.exec(`git config user.email "${user.email}"`);
 
 const dayIndex = differenceInDays(today, startingSunday);
 const commitsPerDay = [...message]
   .flatMap((letter) => letters[letter].flatMap((line) => [...line]))
-  .map((item) => (item === "#" ? 10 : 0));
+  .map((item) => (item === "#" ? NUMBER_OF_COMMITS : 0));
 
 if (dayIndex >= commitsPerDay.length) {
   const daysToNextSunday = 7 - today.getDay() || 7;
   const nextStartingSunday = addDays(new Date(today), daysToNextSunday);
 
+  shell.echo(
+    `Setting new starting sunday to ${format(nextStartingSunday, "yyyy-MM-dd")}`
+  );
   shell.exec(
     `echo "${format(nextStartingSunday, "yyyy-MM-dd")}"  > startingSunday.txt`
   );
@@ -42,10 +47,11 @@ if (dayIndex >= commitsPerDay.length) {
 }
 
 for (let i = 0; i < commitsPerDay[dayIndex]; i++) {
-  const message = `gh commit #${i + 1} on ${today.toLocaleString()}`;
+  const message = `commit #${i + 1} on ${format(today, "yyyy-MM-dd")}`;
   shell.exec(`echo "${message}" >> log.txt`);
   shell.exec("git add .");
   shell.exec(`git commit -m '${message}'`);
 }
 
 shell.exec("git push");
+shell.echo(`Completed commits for today`);
